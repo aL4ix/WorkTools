b47a1fac4f11e5d97d53aced804ad8b88730ebf2
Alan Lima <alan.limagalvez@yahoo.com>
2024-10-16 15:32:39 -0600

    Improving jira_open_from_clipboard code

--- jira_open_from_clipboard.py
+++ jira_open_from_clipboard.py
@@ -12,11 +12,16 @@ LIST_OF_PROJECTS = ['BE', 'FE']
 
 
 def main():
+    if HOST == '':
+        print('Please define a HOST')
+        return
+
     print('Watching for jira links')
+    projects = '|'.join(LIST_OF_PROJECTS)
     while True:
         clipboard = pyperclip.paste()
-        projects = '|'.join(LIST_OF_PROJECTS)
         if re.match(r'('+projects+r')-\d+(\r\n)?$', clipboard):
+            print(clipboard)
             pyperclip.copy('')
             webbrowser.open(
                 f'https://{HOST}.atlassian.net/browse/{clipboard}')
